550bedf82fc07a1c4fcec1e4f7861052da141eda
DilankaEikshana <96680018+DilankaEikshana@users.noreply.github.com>
2026-01-06 10:14:21 +0530

    removed test commands.

--- src/client/java/com/loren/chatsettings/ChatSettingsClient.java
+++ src/client/java/com/loren/chatsettings/ChatSettingsClient.java
@@ -2,11 +2,8 @@ package com.loren.chatsettings;
 
 import com.loren.chatsettings.features.Filter;
 import com.loren.chatsettings.commands.CSCommands;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.network.chat.Component;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,19 +24,5 @@ public class ChatSettingsClient implements ClientModInitializer {
 
         CSCommands.init();
         Filter.init();
-
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(LiteralArgumentBuilder.<FabricClientCommandSource>literal("foo")
-                .executes(context -> {
-                            context.getSource().sendFeedback(Component.literal("Called foo without bar"));
-                            return 1;
-                        }
-                )
-                .then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("bar")
-                        .executes(context -> {
-                            context.getSource().sendFeedback(Component.literal("Called foo with bar"));
-                            return 1;
-                        })
-                )
-        ));
     }
 }
